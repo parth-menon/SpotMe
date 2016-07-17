aea81661f491ecee30060e4acadb140e8d2d2582
ParthasarathyMenon <steelsty@Parthasarathys-MacBook-Pro.local>
2016-07-17 15:36:40 +0530

    MultiDex

--- app/src/main/java/com/steelsty/spotme/MyApp.java
+++ app/src/main/java/com/steelsty/spotme/MyApp.java
@@ -4,24 +4,27 @@ import android.app.Application;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
 /**
  * Created by steelsty on 16/07/16.
  */
 
-public class MyApp extends Application {
+public class MyApp extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.e("start","start");
-        Intent in =new Intent(getApplicationContext(),WidgetService.class);
-        startService(in);
-        in = new Intent(getApplicationContext(),NewAppWidget.class);
-        in.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        int[] ids = AppWidgetManager.getInstance(getApplicationContext()).getAppWidgetIds(new ComponentName(getApplicationContext(), NewAppWidget.class));
-        in.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,ids);
-        sendBroadcast(in);
+//        Log.e("start","start");
+//        Intent in =new Intent(getApplicationContext(),WidgetService.class);
+//        startService(in);
+//        in = new Intent(getApplicationContext(),NewAppWidget.class);
+//        in.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+//        int[] ids = AppWidgetManager.getInstance(getApplicationContext()).getAppWidgetIds(new ComponentName(getApplicationContext(), NewAppWidget.class));
+//        in.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,ids);
+//        sendBroadcast(in);
+        MultiDex.install(MyApp.this);
     }
 }
 
