1799bd223f7990a0900b5189fc5380a84aabd3cc
ParthasarathyMenon <steelsty@Parthasarathys-MacBook-Pro.local>
2016-07-19 01:25:16 +0530

    Alarm List

--- app/src/main/java/com/steelsty/spotme/AlarmActivity.java
+++ app/src/main/java/com/steelsty/spotme/AlarmActivity.java
@@ -5,9 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlarmActivity extends AppCompatActivity {
+
+    ListView listView;
+    List<RowItem> rowItems;
+    public static final String[] titles = new String[] { "Strawberry",
+            "Banana", "Orange", "Mixed" };
+
+    public static final String[] descriptions = new String[] {
+            "It is an aggregate accessory fruit",
+            "It is the largest herbaceous flowering plant", "Citrus Fruit",
+            "Mixed Fruits" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +32,26 @@ public class AlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alarm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
+        rowItems = new ArrayList<RowItem>();
+        for (int i = 0; i < titles.length; i++) {
+            RowItem item = new RowItem(titles[i], descriptions[i]);
+            rowItems.add(item);
+        }
+
+        listView = (ListView) findViewById(R.id.lview);
+        CustomListViewAdapter adapter = new CustomListViewAdapter(this,
+                R.layout.list_item, rowItems);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Item " + (position + 1) + ": " + rowItems.get(position),
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
