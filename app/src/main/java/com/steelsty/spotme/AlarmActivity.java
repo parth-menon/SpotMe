ec51efe0f635fc88f0fb02f8cbd48b9cab163724
ParthasarathyMenon <steelsty@Parthasarathys-MacBook-Pro.local>
2016-07-19 00:54:38 +0530

    Alarm Start

--- app/src/main/java/com/steelsty/spotme/AlarmActivity.java
+++ app/src/main/java/com/steelsty/spotme/AlarmActivity.java
@@ -15,13 +15,13 @@ public class AlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alarm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
