9eafc965e61c7fd81c8d6ed0a856ec23785f6549
ParthasarathyMenon <parthasarathymenon@gmail.com>
2016-07-23 16:19:54 +0530

    Test

--- app/src/main/java/com/steelsty/spotme/AlarmService.java
+++ app/src/main/java/com/steelsty/spotme/AlarmService.java
@@ -203,6 +203,21 @@ public class AlarmService extends Service {
             return d;
         }
     }
+    
+    private double getDistance(double lat1, double lat2, double lon1,
+                               double lon2)
+    {
+        double R = 6371;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double dLat1 = Math.toRadians(lat1);
+        double dLat2 = Math.toRadians(lat2);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(dLat1)
+                * Math.cos(dLat1) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+        return d * 1000;
+    }
 
     @Override
     public boolean stopService(Intent name) {
